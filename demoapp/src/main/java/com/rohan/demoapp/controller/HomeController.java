906e1bcb95ef80878461c9af08d216468c9a9784
Rohan Patil <rohanpatil365@gmail.com>
2020-04-02 10:08:25 +0530

    Update HomeController.java

--- demoapp/src/main/java/com/rohan/demoapp/controller/HomeController.java
+++ demoapp/src/main/java/com/rohan/demoapp/controller/HomeController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 	
 	@GetMapping("/")
+    public String showIndexPage() {
+        return "index";
+    }
+	
+	@GetMapping("/home")
     public String showHomePage() {
         return "home";
     }
