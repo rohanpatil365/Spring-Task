63f04db9e1f6e47da8708872671ee9cae0358a9a
Rohan Patil <rohanpatil365@gmail.com>
2020-04-02 15:57:31 +0530

    Modified response as per graph requirement

--- demoapp/src/main/java/com/rohan/demoapp/controller/CauseCodeController.java
+++ demoapp/src/main/java/com/rohan/demoapp/controller/CauseCodeController.java
@@ -14,15 +14,25 @@ import com.rohan.demoapp.service.CauseCodeService;
 public class CauseCodeController {
 	
 	private ArrayList<Map<String,Object>> dataResponse = null;
+	private Map<String,Object> response = null;
 	
 	@Autowired
 	private CauseCodeService causeCodeService;
 	
+	/*
 	@GetMapping(value = "/cc_report/{account}")
 	public ArrayList<Map<String,Object>> getCauseCodeCount(@PathVariable(value = "account") String account)
 	{
 		dataResponse = causeCodeService.getCauseCodeCount(account);
 		return dataResponse;
+	}
+	*/
+	
+	@GetMapping(value = "/cc_report/{account}")
+	public Map<String,Object> getCCCount(@PathVariable(value = "account") String account)
+	{
+		response = causeCodeService.getCCCount(account);
+		return response;
 	}
 	
 }
--- demoapp/src/main/java/com/rohan/demoapp/service/CauseCodeService.java
+++ demoapp/src/main/java/com/rohan/demoapp/service/CauseCodeService.java
@@ -2,6 +2,7 @@ package com.rohan.demoapp.service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import com.rohan.demoapp.utils.EntityManagerUtils;
 @Service
 public class CauseCodeService {
 	private ArrayList<Map<String,Object>> dataResponse = null;
+	private Map<String,Object> response = null;
 	
 	@Autowired
 	private CauseCodeRepository repository;
@@ -56,6 +58,50 @@ public class CauseCodeService {
 			
 		}
 		return dataResponse;
+	}
+	
+	public Map<String,Object> getCCCount(String account)
+	{
+		response = new HashMap<String,Object>();
+		List<String> accountList = new ArrayList<String>();
+		List<Integer> countList = new ArrayList<Integer>();
+		if("all".equalsIgnoreCase(account))
+		{
+			for(int i=0;i<AccountConstants.ACCOUNT_LIST.length;i++)
+			{
+				String accountName = AccountConstants.ACCOUNT_LIST[i];
+				setRepository(i);
+				int count = repository.getTotalCount();
+				accountList.add(accountName);
+				countList.add(count);
+			}
+		}
+		else
+		{
+			int index = -1;
+			index = AccountConstants.getAccountIndex(account);
+			
+			if(index != -1)
+			{
+				String accountName = AccountConstants.ACCOUNT_LIST[index];
+				setRepository(index);
+				int count = repository.getTotalCount();
+				accountList.add(accountName);
+				countList.add(count);
+			}	
+		}
+		if(accountList.size() >= 1)
+		{
+			response.put("success", true);
+			response.put("accountList", accountList);
+			response.put("countList", countList);
+		}
+		else
+		{
+			response.put("success", false);
+		}
+		
+		return response;
 	}
 	
 	private void setRepository(int id){
