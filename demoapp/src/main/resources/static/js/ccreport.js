6a071b3757f1ebf126fdcb3e15d373bf864c2d7e
Rohan Patil <rohanpatil365@gmail.com>
2020-04-03 20:07:18 +0530

    Added hardcoded data in JS

--- demoapp/src/main/resources/static/js/ccreport.js
+++ demoapp/src/main/resources/static/js/ccreport.js
@@ -2,16 +2,91 @@ $(document).ready(function(){
 	
 	// CC Report Graph Function
 	var cc_report_canvas;
+	
 	function showCCReport() {
+		$("#cc_report_canvas").hide();
 		$("#cc_report_spinner").show();
-		
 		var cc_report_ctx = document.getElementById('cc_report_canvas');
 	    if(cc_report_canvas != undefined) {
 	        
 	    	cc_report_canvas.destroy(); 
 	    }
-	    var accountList;
+	    var labelList;
 	    var countList;
+	    var datasetList = []
+	    
+	    var sector = $("#sector-select option:selected").val();
+	    console.log("sector :"+sector)
+	    if(sector == "sector1")
+	    {
+	    	labelList = ["s1-Account1","s1-Account2","s1-Account3"];
+	    	countList = [50,10,22];
+	    	var obj = {
+	    		label: "causecode count",
+                data: countList,
+                backgroundColor: "#4e73df",
+                maxBarThickness: 100,
+	    	};
+	    	datasetList.push(obj);
+	    	$("#cc_report_x").text("account");
+	    }
+	    else if(sector == "sector2")
+	    {
+	    	labelList = ["s2-Account1","s2-Account2","s2-Account3","s2-Account4"];
+	    	countList = [5,31,14,8];
+	    	var obj = {
+	    		label: "causecode count",
+                data: countList,
+                backgroundColor: "#4e73df",
+                maxBarThickness: 100,
+	    	};
+	    	datasetList.push(obj);
+	    	$("#cc_report_x").text("account");
+	    }
+	    else if(sector == "sector3")
+	    {
+	    	labelList = ["s3-Account1","s3-Account2","s3-Account3","s3-Account4","s3-Account5","s3-Account5"];
+	    	countList = [12,25,5,31,14,8];
+	    	var obj = {
+	    		label: "causecode count",
+                data: countList,
+                backgroundColor: "#4e73df",
+                maxBarThickness: 100,
+	    	};
+	    	datasetList.push(obj);
+	    	$("#cc_report_x").text("account");
+	    }
+	    else if(sector == "all" || sector == "-1")
+	    {
+	    	labelList = ["sector1","sector2","sector3"];
+	    	var obj1 =  {label:"s1-Account1",data:[50,0,0],backgroundColor:"#00876c"};
+	    	var obj2 =  {label:"s1-Account2",data:[10,0,0],backgroundColor:"#89bf77"};
+	    	var obj3 =  {label:"s1-Account3",data:[22,0,0],backgroundColor:"#fff18f"};
+	    	var obj4 =  {label:"s2-Account1",data:[0,5,0],backgroundColor:"#00876c"};
+	    	var obj5 =  {label:"s2-Account2",data:[0,31,0],backgroundColor:"#89bf77"};
+	    	var obj6 =  {label:"s2-Account3",data:[0,14,0],backgroundColor:"#fff18f"};
+	    	var obj7 =  {label:"s2-Account4",data:[0,8,0],backgroundColor:"#f59b56"};
+	    	var obj8 =  {label:"s3-Account1",data:[0,0,12],backgroundColor:"#00876c"};
+	    	var obj9 =  {label:"s3-Account2",data:[0,0,25],backgroundColor:"#89bf77"};
+	    	var obj10 = {label:"s3-Account3",data:[0,0,5],backgroundColor:"#fff18f"};
+	    	var obj11 = {label:"s3-Account4",data:[0,0,31],backgroundColor:"#f59b56"};
+	    	var obj12 = {label:"s3-Account5",data:[0,0,14],backgroundColor:"#d43d51"};
+	    	datasetList.push(obj1);
+	    	datasetList.push(obj2);
+	    	datasetList.push(obj3);
+	    	datasetList.push(obj4);
+	    	datasetList.push(obj5);
+	    	datasetList.push(obj6);
+	    	datasetList.push(obj7);
+	    	datasetList.push(obj8);
+	    	datasetList.push(obj9);
+	    	datasetList.push(obj10);
+	    	datasetList.push(obj11);
+	    	datasetList.push(obj12);
+	    	$("#cc_report_x").text("sector");
+	    }
+	    
+	    /*
 	    $.ajax({
 		    async: false,
 		    type: "GET",
@@ -23,28 +98,22 @@ $(document).ready(function(){
 		        console.log("Inside AJAX : accountList - "+accountList);
 		    }
 		});
-	    
-	    console.log("Outside AJAX : accountList - "+accountList);
+		*/
 	    
 	    cc_report_canvas = new Chart(cc_report_ctx, {
 	       type: 'bar',
 	       data: {
-	           labels:accountList,
-	           datasets: [
-	               {
-	                   label: "Count",
-	                   data: countList,
-	                   backgroundColor: "#4e73df",
-	                   maxBarThickness: 100,
-	               }
-	           ]
+	           labels:labelList,
+	           datasets: datasetList
 	       },
 	       options: {
 	  	        scales: {
 	  	        	xAxes: [{
+	  	        		stacked:true,
 	  	        		maxBarThickness: 50,
 	  	            }],
 	  	            yAxes: [{
+	  	            	stacked:true,
 	  	                ticks: {
 	  	                    beginAtZero: true,
 	  	                    maxTicksLimit: 6,
@@ -60,7 +129,7 @@ $(document).ready(function(){
 	    });
 	    
 	    $("#cc_report_spinner").hide();
-	    
+	    $("#cc_report_canvas").show();
 	}
 	
 	function showDonutChart()
@@ -95,6 +164,15 @@ $(document).ready(function(){
 		    cutoutPercentage: 80,
 		  },
 		});
+	}
+	
+	$("#sector-select").change(function () {
+		showCCReport();
+	});
+	
+	function randomHexColor()
+	{
+		return '#'+Math.floor(Math.random()*16777215).toString(16);
 	}
 	
 	showCCReport();
